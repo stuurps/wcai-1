38b77e17dd7f71f5572a5ebdd5f73fbe42e90b8e
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2019-08-18 08:47:02 +0000

    Add nearestPlace info to popup

--- js/wcaiMarker.js
+++ js/wcaiMarker.js
@@ -4,7 +4,7 @@ class wcaiMarker {
     constructor(lat, lng, map) {
         this.lat = lat;
         this.lng = lng;
-        this.w3wData = {};
+        this.w3wData = null;
         this.init(map);
     }
 
@@ -12,14 +12,15 @@ class wcaiMarker {
         console.log('Getting w3w data');
         const responseObject = await what3words.api.convertTo3wa({lat:this.lat, lng:this.lng});
         this.w3wData = responseObject;
-        console.log("this.w3wData:")
         console.log(this.w3wData);
         this.addToMap(map);
     }
 
     addToMap = (map) => {
         let marker = L.marker([this.lat, this.lng], this.w3wData).addTo(map);
-        const html = `<b>${this.w3wData.words}</b>`
+        const header = `<b>${this.w3wData.words}</b>`;
+        const body = `<p>${this.w3wData.nearestPlace}</p>`;
+        const html = `${header}<br>${body}`;
         marker.bindPopup(html).openPopup();
     }
 
