9bd790f5c21e03f908155dff6183dc5d2fde602e
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2019-08-18 09:04:47 +0000

    Add logic for constructing marker from existing site data

--- js/wcaiMarker.js
+++ js/wcaiMarker.js
@@ -1,32 +1,42 @@
 console.log("wcaiMarker.js connected");
 
 class wcaiMarker {
-    constructor(lat, lng, map) {
+    constructor(lat = null, lng = null, map, siteData = null) {
         this.lat = lat;
         this.lng = lng;
-        this.w3wData = null;
-        this.init(map);
+        this.siteData = siteData;       // Will be null for new sites
+        this.isNewSite = this.siteData == null;
+        if (this.isNewSite) {
+            this.initFromScratch(map);  // Get w3w data
+        } else {
+            this.addToMap(map);            // siteData contains all we need
+        }
     }
 
-    init = async (map) => {
+    initFromScratch = async (map) => {
         console.log('Getting w3w data');
         const responseObject = await what3words.api.convertTo3wa({lat:this.lat, lng:this.lng});
-        this.w3wData = responseObject;
-        console.log(this.w3wData);
+        this.siteData = responseObject;
+        console.log(this.siteData);
         this.addToMap(map);
     }
 
     addToMap = (map) => {
-        let marker = L.marker([this.lat, this.lng], this.w3wData).addTo(map);
+        let marker = L.marker([this.lat, this.lng], this.siteData).addTo(map);
         const markerHTML = this.buildPopup();
         marker.bindPopup(markerHTML).openPopup();
     }
 
     buildPopup = () => {
-        const header = `<b>${this.w3wData.words}</b>`;
-        const body = `<p>${this.w3wData.nearestPlace}</p>`;
+        const header = `<b>${this.siteData.words}</b>`;
+        const body = `<p>${this.siteData.nearestPlace}</p>`;
         const html = `${header}<br>${body}`;
         return html;
+    }
+
+    saveData = () => {
+        // Take the input data and save to the db
+        pass
     }
 
 }
